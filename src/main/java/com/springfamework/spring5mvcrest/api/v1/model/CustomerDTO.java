31d9d475f4fcfb6c66ca388ea6d93accdb6ecf43
Joao Moreira <jfmoreir@thoughtworks.com>
2018-01-02 19:59:12 -0200

    Adding swagger support

--- src/main/java/com/springfamework/spring5mvcrest/api/v1/model/CustomerDTO.java
+++ src/main/java/com/springfamework/spring5mvcrest/api/v1/model/CustomerDTO.java
@@ -1,12 +1,15 @@
 package com.springfamework.spring5mvcrest.api.v1.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
 
+    @ApiModelProperty(value = "This is the first name", required = true)
     private String firstName;
 
+    @ApiModelProperty(required = true)
     private String lastName;
 
     private String url;
